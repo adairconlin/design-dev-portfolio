df177d7bf7beb57d87736b3dd1bb554f791b1d21
Adair Conlin <adair.conlin@gmail.com>
2020-11-25 15:35:45 -0500

    removed excess files

--- js/Logo Design.js
+++
@@ -1,7 +0,0 @@
-jQuery(function(){
-	jQuery('.showSingle').click(function(e){
-		  jQuery('.targetDiv').hide();
-		  jQuery('#section'+$(this).attr('target')).show();
-		  $("#dropdownBtn").html($(e.target).text());
-	});
-});
