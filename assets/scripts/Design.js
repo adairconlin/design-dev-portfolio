d6d41e7cf459be8a292c546c96c93d78b4be8c80
adairconlin <adairconlin@gmail.com>
2021-10-11 12:35:33 -0400

    redesigned BrandDesign.html

--- assets/scripts/Design.js
+++ assets/scripts/Design.js
@@ -1,7 +1,27 @@
-jQuery(function(){
-	jQuery('.showSingle').click(function(e){
-		  jQuery('.targetDiv').hide();
-		  jQuery('#section'+$(this).attr('target')).show();
-		  $("#dropdownBtn").html($(e.target).text());
-	});
-});
+function getId(btn) {
+	let target = btn.id;
+	let targetSection = "section " + target;
+
+	/* Changes active button styles */
+	for(i = 1; i < 4; ++i) {
+		let e = document.getElementById(i);
+		if(e.className === "active") {
+			e.classList.remove("active");
+		}
+	}
+	let x = document.getElementById(target);
+	x.classList.add("active");
+
+	/* Shows targeted section */
+	let showDiv = document.getElementById(targetSection);
+	if(showDiv.className === "hide") {
+		let hideDiv = document.querySelector(".show");
+		/* Hides active section */
+		hideDiv.classList.remove("show");
+		hideDiv.classList.add("hide");
+		/* Displays new section */
+		showDiv.classList.remove("hide");
+		showDiv.classList.add("show");
+	}
+
+}
