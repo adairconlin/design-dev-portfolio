4a4667dedc73f66e5574d29b312de6c39d1d4399
adairconlin <adairconlin@gmail.com>
2021-10-13 14:08:58 -0400

    removed unnecessary files and code

--- assets/scripts/Digital Portraits.js
+++
@@ -1,9 +0,0 @@
-function hideSketches(idName) {
-	var x = document.getElementById(idName);
-	console.log(idName);
-	if (x.style.display === "none") {
-		x.style.display = "flex";
-	} else {
-		x.style.display = "none";
-	}
-}
