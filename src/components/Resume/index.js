ce2847092fe9ced23fde8445c90c11209c7e00dc
adairconlin <adair.conlin@gmail.com>
2022-02-23 12:30:22 -0500

    Finish resume effects

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,33 +7,33 @@ function Resume() {
             <h1 className="hText lg">My current skills.</h1>
             <div>
                 <section>
-                    <h2 className="hText lg accent">Front End</h2>
+                    <h2 className="hText md accent">Front End</h2>
                     <ul className="hDisplay sm">
-                        <li>HTML5</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>jQuery</li>
-                        <li>React.js</li>
-                        <li>Bootstrap</li>
-                        <li>Bulma</li>
-                        <li>Responsive web design</li>
-                        <li>Accessible web design</li>
+                        <li><span>/* </span>HTML5 <span>*/</span></li>
+                        <li><span>/* </span>CSS <span>*/</span></li>
+                        <li><span>/* </span>JavaScript <span>*/</span></li>
+                        <li><span>/* </span>React.js <span>*/</span></li>
+                        <li><span>/* </span>jQuery <span>*/</span></li>
+                        <li><span>/* </span>Bootstrap <span>*/</span></li>
+                        <li><span>/* </span>Bulma <span>*/</span></li>
+                        <li><span>/* </span>Responsive web design <span>*/</span></li>
+                        <li><span>/* </span>Accessible web design <span>*/</span></li>
                     </ul>
                 </section>
                 <section>
-                    <h2 className="hText lg accent">Back End</h2>
+                    <h2 className="hText md accent">Back End</h2>
                     <ul className="hDisplay sm">
-                        <li>Node.js</li>
-                        <li>Express.js</li>
-                        <li>MySQL</li>
-                        <li>Sequelize.js</li>
-                        <li>NoSQL</li>
-                        <li>MongoDB</li>
+                        <li><span>// </span>Node.js</li>
+                        <li><span>// </span>Express.js</li>
+                        <li><span>// </span>MySQL</li>
+                        <li><span>// </span>Sequelize.js</li>
+                        <li><span>// </span>NoSQL</li>
+                        <li><span>// </span>MongoDB</li>
                     </ul>
                 </section>
                 <section>
-                    <h2 className="hText lg accent">Technology</h2>
-                    <ul className="hDisplay sm">
+                    <h2 className="hText md accent">Technology</h2>
+                    <ul className="hDisplay sm tech">
                         <li>Git</li>
                         <li>GitHub</li>
                         <li>Heroku</li>
