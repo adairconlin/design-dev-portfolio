308cdac4ee57274637c1fdc79fd4582597971461
adairconlin <adair.conlin@gmail.com>
2022-02-22 21:54:46 -0500

    Finish portfolio components

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -22,6 +22,27 @@ function Project() {
             projectLink: "https://github.com/adairconlin/e-commerce-backend",
             githubLink: "https://github.com/adairconlin/e-commerce-backend",
             src: "e-commerce"
+        },
+        {
+            title: "Weather Dashboard",
+            subtitle: "Weather API",
+            projectLink: "https://adairconlin.art/weather-dashboard/",
+            githubLink: "https://github.com/adairconlin/weather-dashboard",
+            src: "weather-dash"
+        },
+        {
+            title: "Team Profile Generator",
+            subtitle: "CLI Application",
+            projectLink: "https://github.com/adairconlin/team-profile-generator",
+            githubLink: "https://github.com/adairconlin/team-profile-generator",
+            src: "team-gen"
+        }, 
+        {
+            title: "Food Mood",
+            subtitle: "Meal & Drink Application",
+            projectLink: "https://adairconlin.art/food-mood/",
+            githubLink: "https://github.com/adairconlin/food-mood",
+            src: "food-mood"
         }
     ])
 
@@ -29,14 +50,8 @@ function Project() {
         <>
             {images.map((image) => (
                 <div className="hover">
-                    <img
-                        src={require(`../../assets/colored/${image.src}.png`)}
-                        alt={image.title}
-                        key={`${image.title} colored`}
-                        className="project colored inner-img"
-                    />
                     <div className="colored project-hover inner-div">
-                        <h1 className="hText lg-dis">{image.title}</h1>
+                        <h1 className="hText lg-dis"><a href={image.projectLink}>{image.title}</a></h1>
                         <h2 className="hText prj-sub">{image.subtitle}</h2>
                         <ul className="hDisplay prj-link">
                             <li><a href={image.projectLink}>View Project</a></li>
@@ -44,10 +59,16 @@ function Project() {
                         </ul>
                     </div>
                     <img
+                        src={require(`../../assets/colored/${image.src}.png`)}
+                        alt={image.title}
+                        key={`${image.title} colored`}
+                        className="project"
+                    />
+                    <img
                         src={require(`../../assets/original/${image.src}.png`)}
                         alt={image.title}
                         key={`${image.title} original`}
-                        className="project inner-original"
+                        className="project colored inner-img"
                     />
                 </div>
             ))}
