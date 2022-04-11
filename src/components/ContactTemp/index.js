ae6baca66a642654924b14b4544520e446018c7f
adairconlin <adair.conlin@gmail.com>
2022-04-11 18:45:52 -0400

    Added temporary contact, changes links to target _blank

---
+++ src/components/ContactTemp/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactTemp = () => {
+    return (
+        <main className="contact">
+            <h1 className="hDisplay md">Contact form coming soon!</h1>
+            <a href="mailto:adair.conlin@gmail.com" className="hDisplay sm">Email me at: adair.conlin@gmail.com.</a> <br />
+            <a href="http://linkedin.com/in/adair-conlin" target="_blank" className="hDisplay sm">Connect with me on LinkedIn.</a>
+        </main>
+    )
+}
+
+export default ContactTemp;
